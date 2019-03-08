89000971f2d8128ce9e5d801ed6aecc58184419e
Julian Gomez <andresjuliangomez@hotmail.com>
2019-03-07 21:36:13 -0500

    Prueba2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Prueba");
+            Console.WriteLine("Prueba 2");
             Console.ReadLine();
         }
     }
